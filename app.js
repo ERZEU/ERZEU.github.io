752d603ec1d98152a0ba15da26cffb1b6015d0ab
ERZEU <47894340+ERZEU@users.noreply.github.com>
2023-07-28 14:21:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ tg.expand();
 let btn = document.getElementById("sb");
 
 btn.addEventListener("click", function(){
-    let firstname = document.getElementById("firstname");
-    let lastname = document.getElementById("lastname");
-    let email = document.getElementById("email");
+    let firstname = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
     let data = {
         firstname: firstname,
         lastname: lastname,
