6d1a52ac76f812fa244459e73e65c0452ec7871d
ERZEU <47894340+ERZEU@users.noreply.github.com>
2023-07-28 14:13:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,16 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let firstname = "1";
-let lastname = "2";
-let email = "3";
-
 let btn = document.getElementById("sb");
 
 btn.addEventListener("click", function(){
-    firstname = document.getElementById("firstname");
-    lastname = document.getElementById("lastname");
-    email = document.getElementById("email");
+    let firstname = document.getElementById("firstname");
+    let lastname = document.getElementById("lastname");
+    let email = document.getElementById("email");
     let data = {
-        fname: firstname,
-        lname: lastname,
-        mail: email
+        firstname: firstname,
+        lastname: lastname,
+        email: email
     }
     tg.sendData(JSON.stringify(data));
     tg.close();
