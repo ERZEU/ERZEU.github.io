cd43ef51f28a800211fae5024d7558f52fe741b6
ERZEU <47894340+ERZEU@users.noreply.github.com>
2023-07-28 13:54:57 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let firstname = "";
-let lastname = "";
-let email = "";
+let firstname = "1";
+let lastname = "2";
+let email = "3";
 
 let btn = document.getElementById("sb");
 
@@ -12,8 +12,15 @@ btn.addEventListener("click", function(){
     firstname = document.getElementById("firstname");
     lastname = document.getElementById("lastname");
     email = document.getElementById("email");
+    tg.close();
 });
 
+let data = {
+    fname: firstname,
+    lname: lastname,
+    mail: email
+}
+
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(firstname, lastname, email);
+    tg.sendData(JSON.stringify(data));
 });
