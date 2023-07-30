2066aa9ad2d3ba11763291dd1c0d5c0b6403db01
ERZEU <47894340+ERZEU@users.noreply.github.com>
2023-07-30 21:35:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ tg.expand();
 
 let btn = document.getElementById("sb");
 
-function getBase64(file) {
-   var reader = new FileReader();
-   reader.readAsDataURL(file[0]);
-   reader.onload = function () {
-     return reader.result;
-   };
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+});
+
 
 btn.addEventListener("click", function(){
     let firstname = document.getElementById("firstname").value;
     let lastname = document.getElementById("lastname").value;
     let email = document.getElementById("email").value;
-    let file = document.getElementById("file").files;
-    let temp = getBase64(file)
+    let file = document.querySelector('#files > input[type="file"]').files[0];
+    let temp = toBase64(file);
     let data = {
         firstname: firstname,
         lastname: lastname,
